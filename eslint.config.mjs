e5689df37f4770d5e9e8722e3efd984312403477
ehh99 <jinung5@gmail.com>
2024-06-20 09:17:26 +0900

    6.19 DOM 수업

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,14 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+const type = {
+  isString: true,
+  isObject: true,
+  isArray: true,
+  isNull: true,
+  getNode: true,
+  getNodes: true,
+};
 
 export default [
   pluginJs.configs.recommended,
@@ -8,10 +17,17 @@ export default [
       globals: {
         ...globals.browser,
         ...globals.node,
+        ...type,
+        setAttr: true,
+        getAttr: true,
+        insertBefore: true,
+        insertFirst: true,
+        insertLast: true,
+        insertAfter: true,
       },
     },
     rules: {
-      "no-unused-vars": "warn",
+      'no-unused-vars': 'warn',
     },
   },
 ];
